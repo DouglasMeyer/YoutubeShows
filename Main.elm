7bbec9def584fa4a6ebaf1eb0793ec04b583a7ee
Doug Meyer <doug.meyer@vibes.com>
2016-09-13 16:57:09 -0500

    Don't fetch videos while channels is fetching.
    And show loading line.

--- Main.elm
+++ Main.elm
@@ -4,10 +4,9 @@ import Html exposing (..)
 import Html.App as App
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onInput)
-import Html.Lazy exposing (lazy2)
+import Html.Lazy exposing (lazy2, lazy3)
 import Time exposing (Time)
 import Dict exposing (Dict)
-import Task
 import Regex
 
 main : Program (Maybe StoredState)
@@ -51,7 +50,9 @@ doSetStorage model cmds =
 type alias Model =
   { authorization : Maybe Authorization
   , lastAuthCheck : Time
+  , isFetchingChannels : Bool
   , lastChannelFetch : Time
+  , isFetchingVideos : Bool
   , lastVideosFetch : Time
   , timeTillNextVideosFetch : Float
   , channels : Dict String Channel
@@ -90,7 +91,9 @@ emptyModel : Model
 emptyModel =
   { authorization = Nothing
   , lastAuthCheck = 0
+  , isFetchingChannels = False
   , lastChannelFetch = 0
+  , isFetchingVideos = False
   , lastVideosFetch = 0
   , timeTillNextVideosFetch = 0
   , channels = Dict.empty
@@ -108,9 +111,7 @@ init startingState =
       { emptyModel
         | authorization = Nothing
         , channels = addChannelsToChannels stored.channels emptyModel.channels
-      } ! [ Time.now
-        |> Task.perform (\now -> NoOp) (\now -> AuthWithToken stored.authorization now)
-      ]
+      } ! []
 
 
 -- UPDATE
@@ -120,7 +121,9 @@ type Msg
   | Tick Time
   | AuthWithToken (Maybe Authorization) Time
   | SetAuthorization ( Maybe Authorization )
+  | ChannelFetchComplete
   | AddSubscribedChannels ( List Channel )
+  | VideoFetchComplete
   | AddVideos ( List Video )
   | FilterChannels String
   | FilterVideos String
@@ -144,9 +147,10 @@ update msg model =
         else if isAuthorized && (now - model.lastChannelFetch) > Time.hour * 16 then
           { model
             | lastChannelFetch = now
+            , isFetchingChannels = True
             , timeTillNextVideosFetch = timeTillNextVideosFetch
           } ! [ requestYTSubscribedChannels () ]
-        else if isAuthorized && timeTillNextVideosFetch <= 0 then
+        else if isAuthorized && not model.isFetchingChannels && timeTillNextVideosFetch <= 0 then
           let
             channelPlaylistIds = model.channels
               |> Dict.values
@@ -154,6 +158,7 @@ update msg model =
           in
             { model
               | lastVideosFetch = now
+              , isFetchingVideos = True
               , timeTillNextVideosFetch = timeTillNextVideosFetch
             } ! [ requestYTVideos channelPlaylistIds ]
         else
@@ -169,11 +174,17 @@ update msg model =
     SetAuthorization authorization ->
       { model | authorization = authorization } ! []
 
+    ChannelFetchComplete ->
+      { model | isFetchingChannels = False } ! []
+
     AddSubscribedChannels channels ->
       { model
         | channels = addChannelsToChannels channels model.channels
         , lastVideosFetch = 0
       } ! []
+
+    VideoFetchComplete ->
+      { model | isFetchingVideos = False } ! []
 
     AddVideos videos ->
       { model
@@ -229,7 +240,9 @@ port gotAuthorization : (Authorization -> msg) -> Sub msg
 port failedAuthorization : (() -> msg) -> Sub msg
 
 port subscribedChannels : (List Channel -> msg) -> Sub msg
+port channelFetchComplete : (() -> msg) -> Sub msg
 port channelVideos : (List Video -> msg) -> Sub msg
+port videoFetchComplete : (() -> msg) -> Sub msg
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -238,7 +251,9 @@ subscriptions model =
     , gotAuthorization (\authorization -> SetAuthorization (Just authorization))
     , failedAuthorization (\_ -> SetAuthorization Nothing)
     , subscribedChannels AddSubscribedChannels
+    , channelFetchComplete (\_ -> ChannelFetchComplete)
     , channelVideos AddVideos
+    , videoFetchComplete (\_ -> VideoFetchComplete)
     ]
 
 
@@ -255,36 +270,27 @@ view model =
         |> List.filter (\channel -> Regex.contains filterRegex channel.title)
   in
     div
-      [ class "vertical-layout" ]
-      [ viewHeader model
-      , div
-        [ class "expand-layout horizontal-layout scroll-vertically"
-        ]
-        [ lazy2 viewChannelList model.channelFilter filteredChannels
-        , lazy2 viewVideosList filteredChannels model.videoFilter
-        ]
-      , footer [] [ text "by Douglas Meyer" ]
+      [ class "horizontal-layout"
       ]
-
-viewHeader : Model -> Html Msg
-viewHeader model =
-  div
-    [ class "horizontal-layout do-not-shrink" ]
-    [ header [] [ text "YoutubeShows" ]
-    , div [ class "expanded-layout align-text-center" ] [ text ("Next fetch " ++ toString (round (model.timeTillNextVideosFetch / 1000))) ]
-    , div [ class "expand-layout align-text-right" ] [ text (if model.authorization /= Nothing then "Authorized" else "Not Authorized") ]
-    ]
+      [ lazy2 viewChannelList model.channelFilter filteredChannels
+      , lazy3
+        viewVideosList
+        filteredChannels
+        ( model.isFetchingChannels || model.isFetchingVideos )
+        model.videoFilter
+      ]
 
 viewChannelList : String -> List Channel -> Html Msg
 viewChannelList channelFilter channels =
   ul [ class "no-list horizontal-margin" ] <|
     ( li
-      [ class "vertical-margin" ]
+      []
       [ input
         [ type' "search"
         , placeholder "channel filter"
         , onInput FilterChannels
         , value channelFilter
+        , style [ ("width", "100%") ]
         ]
         []
       ]
@@ -292,16 +298,26 @@ viewChannelList channelFilter channels =
       (\channel ->
         li
           [ onClick (FilterChannels channel.title), class (if channel.title == channelFilter then "selected vertical-margin" else "clickable vertical-margin")
-          , style [("font-size", "0")]
+          , style
+            [ ("background", "url(" ++ channel.thumbnailUrl ++ ") no-repeat center / cover")
+            , ("padding-top", "calc(88px - 1em)")
+            , ("width", "10em")
+            ]
           ]
-          [ img [ src channel.thumbnailUrl, title channel.title, alt channel.title, width 88, height 88 ] []
-          , div [ style [("font-size", "1rem")] ] [ text channel.title ]
+          [ div
+            [ style
+              [ ("color", "#EEE")
+              , ("background-image", "-webkit-linear-gradient(bottom, black, black, transparent)")
+              , ("padding", "2px 5px")
+              ]
+            ]
+            [ text channel.title ]
           ]
       )
       channels
 
-viewVideosList : List Channel -> String -> Html Msg
-viewVideosList channels videoFilter =
+viewVideosList : List Channel -> Bool -> String -> Html Msg
+viewVideosList channels isFetching videoFilter =
   let
     filterRegex = videoFilter |> Regex.regex |> Regex.caseInsensitive
     videos = channels
@@ -312,7 +328,7 @@ viewVideosList channels videoFilter =
   in
     ul [ class "expand-layout no-list horizontal-margin"] <|
       ( li
-        [ class "vertical-margin" ]
+        []
         [ input
           [ type' "search"
           , placeholder "video filter"
@@ -321,24 +337,57 @@ viewVideosList channels videoFilter =
           ]
           []
         ]
-      ) :: List.map
-        (\video ->
-          let
-            defaultChannelImgAttrs = [ width 88, height 88 ]
-            channelImgAttrs = case findById video.channelId channels of
-              Nothing ->
-                src "//s.ytimg.com/yts/img/avatar_720-vflYJnzBZ.png" :: class "horizontal-margin" :: defaultChannelImgAttrs -- FIXME: need a better way to get default channel
-              Just channel ->
-                src channel.thumbnailUrl :: onClick (FilterChannels channel.title) :: class "horizontal-margin clickable" :: alt channel.title :: defaultChannelImgAttrs
-          in
-            li
-              [ class "horizontal-layout vertical-margin" ]
-              [ img channelImgAttrs []
-              , img [ src video.thumbnailUrl, width 120, height 90, class "horizontal-margin" ] []
-              , h3 [ class "horizontal-margin" ] [ text video.title ]
-              ]
-        )
-        videos
+      ) :: ( li
+        [ style (if isFetching then [] else [("display","none")]) ]
+        [ text "Checking for new videos" ]
+      ) :: List.map (\v -> videoView v channels) videos
+
+videoView : Video -> List Channel -> Html Msg
+videoView video channels =
+  let
+    channel = case findById video.channelId channels of
+      Nothing ->
+        Channel "" "unknown channel" "//s.ytimg.com/yts/img/avatar_720-vflYJnzBZ.png" "" [] []
+      Just channel ->
+        channel
+  in
+    li
+      [ class "vertical-margin"
+      , style
+        [ ("display", "flex")
+        , ("flex-direction", "column")
+        ]
+      ]
+      [ div
+        [ style [ ("display", "flex"), ("align-items","center"), ("margin-bottom","3px") ]
+        , onClick (FilterChannels channel.title)
+        , class "clickable"
+        ]
+        [ img
+          [ style
+            [ ("height","1.2em")
+            , ("margin-right","1ex")
+            ]
+          , src channel.thumbnailUrl
+          , alt channel.title
+          ] []
+        , text channel.title
+        ]
+      , a
+        [ style [ ("display", "flex"), ("align-items","center"), ("font-size","1.1rem"), ("font-weight","bold") ]
+        , href ("http://youtube.com/watch?v=" ++ video.id)
+        , target "_new"
+        ]
+        [ img
+          [ width 120
+          , height 90
+          , style [ ("margin-right","1em") ]
+          , src video.thumbnailUrl
+          ]
+          []
+        , text video.title
+        ]
+      ]
 
 findById : String -> List { a | id : String } -> Maybe { a | id : String }
 findById id list =
